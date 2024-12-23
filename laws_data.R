9aa39e228f60a36e801322fb6c03fefbb9c59a23
AP Pittman <ap.r.pittman@gmail.com>
2024-12-22 20:51:55 -0500

    doing the laws thing correctly this time

--- laws_data.R
+++ laws_data.R
@@ -1,45 +1,57 @@
-#### code for reading in and visualizing the anti-trans laws data
-
-
+## setup
 library(tidyverse)
 library(readxl)
 library(here)
 
-laws_data <- read_xlsx(here("data_raw", "antitranslaws.xlsx")) |> 
-  mutate(passed_date = as.Date(passed_date))
-
-laws_data_merge <- laws_data |> 
-  select(state, passed_date) |> 
-  unique()
+## read in excel data
+## note: this data was obtained from the following websites:
 
 
-laws_data_merge 
+# https://www.aclu.org/documents/past-legislation-affecting-lgbt-rights-across-country-2019
+# https://www.aclu.org/documents/past-legislation-affecting-lgbt-rights-across-country-2020
+# https://www.aclu.org/documents/legislation-affecting-lgbtq-rights-across-country-2021
+# https://www.aclu.org/documents/legislation-affecting-lgbtq-rights-across-country-2022
+# https://www.aclu.org/legislative-attacks-on-lgbtq-rights-2023
 
-load(here("data", "pulsetimeline.Rdata"))
+# in 2018-2022, the ACLU separated the bills into sections, and I only included those in the anti-trans section
+# in 2023, they had to be tagged with one of the following: public accomodations bans, school facilities bans, school sports bans, healthcare restrictions, drag bans, barriers to accessing accurate ID
+
+laws_data <- read_xlsx(here("data_raw", "antitranslaws.xlsx"))
+
+states <- tibble(state.name, state.abb)
+
+laws_data <- left_join(laws_data, states, by = c("state_name" = "state.name")) |>
+  print(n = Inf)
+
+laws_data <- laws_data |>
+  mutate(state = case_when(is.na(state_name) ~ state,
+                           !is.na(state_name) ~ state.abb)) |>
+  select(-c(state_name, state.abb))
+
+# total: 579 laws
+# need to get data about them from LegiScan
+
+laws_data <- laws_data |> 
+  mutate(bill = sub("/.*", "", bill),
+         bill = str_remove_all(bill, "[[:space:]]"),
+         legiscan_url = paste0("https://legiscan.com/",
+                               state,
+                               "/bill/",
+                               bill,
+                               "/",
+                               year)) |> 
+  select(-date)
+
+# from here, looked up each url manually and entered either "dead" or "passed" in the status
+# for bills that were passed, I included the date they were passed
+
+# did a third of these on 12/22/24, here's that data
+
+complete <- read_csv(here("data_raw", "incomplete_laws_data.csv"))
+
+laws_data <- left_join(laws_data, complete)
 
 
-panel <- tibble(state.name) |> 
-  slice(rep(1:n(), each = 30)) |> 
-  mutate(week = rep(34:63, length.out = 1500)) |> 
-  left_join(pulsetimeline) |> 
-  left_join(laws_data_merge, by = c("state.name" = "state")) |> 
-  mutate(treated_state_year = case_when(is.na(passed_date) ~ 0,
-                                        passed_date < enddate ~ 1,
-                                        .default = 0)) |> 
-  group_by(state.name) |> 
-  mutate(number_treated = sum(treated_state_year))
+write_csv(laws_data, file = here("data_raw", "partial_laws_data.csv"))
 
-
-
-ggplot(data = panel) +
-  geom_tile(aes(x = week, y = reorder(state.name, number_treated), 
-                     fill = as.factor(treated_state_year)), color = "black") +
-  scale_fill_manual(values = c("skyblue", "tomato")) +
-  labs(x = "Wave",
-       y = "State",
-       fill = "Treatment Status") +
-  theme_minimal() +
-  theme(legend.position = "bottom")
-
-
-save(panel, file = here("data", "panelsetup.Rdata"))
+#################################### need to do the rest
