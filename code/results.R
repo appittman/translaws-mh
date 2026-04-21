f50cfacd482609d85a1bb6fbbe265213d80721a1
appittman <ap.r.pittman@gmail.com>
2026-04-21 17:13:39 -0400

    organizing results

--- code/results.R
+++ code/results.R
@@ -4,6 +4,9 @@ library(here)
 
 load(here("did_results", "results_0413.Rdata"))
 
+
+################ Calculate treatment effects
+
 get_te <- function(did_result) {
   aggte(did_result,
         type = "dynamic",
@@ -19,7 +22,7 @@ te_trans <- did_trans |> map(get_te)
 te_trans_nsg <- did_trans_nsg |> map(get_te)
 te_trans_cov <- did_trans_cov |> map(get_te)
 
-######## reorganizing for displaying results
+############ reorganizing for displaying results
 
 first_models <- list(trans = te_trans$first,
                      trans_cov = te_trans_cov$first,
@@ -49,51 +52,54 @@ sports_models <- list(trans = te_trans$sports,
                       queer_cov = te_queer_cov$sports,
                       queer_nsg = te_queer_nsg$sports)
 
-get_coeffs <- function(model_list) {
-  atts <- model_list |> 
-    map(pluck("overall.att"))
-  ses <- model_list |> 
-    map(pluck("overall.se"))
-  
-  tibble(model = names(model_list),
-         overall_att = unlist(atts),
-         overall_se = unlist(ses))
-}
+############## Get coefficients from aggte objects
 
+get_coeffs <- function(model_list) {
+
+dynamic <- tibble(
+  t = model_list |> 
+    map(pluck("egt")),
+  att = model_list |> 
+    map(pluck("att.egt")),
+  se = model_list |> 
+    map(pluck("se.egt"))) |> 
+  mutate(modelspec = names(t)) |> 
+  unnest_longer(t:se)
+
+overall <- tibble(
+  overall_att = model_list |> 
+    map(pluck("overall.att")),
+  overall_se = model_list |> 
+    map(pluck("overall.se")),
+  modelspec = names(model_list)) |> 
+  unnest_longer(overall_att:overall_se)
+
+left_join(dynamic, overall, by = "modelspec")
+
+}
 
 coeffs <- map(list(medical = medical_models,
          bathroom = bathroom_models,
          sports = sports_models,
          first = first_models),
     get_coeffs)
-  
-df_coeffs <- tibble(coeffs) |> 
-  unnest_wider(coeffs) |> 
-  mutate(law_type = names(coeffs)) |> 
-  unnest_longer(c(model:overall_se)) |> 
-  select(-ends_with("id"))
 
 
-lawnames <- c("first" = "First Law Overall",
-              "medical" = "First Medical Law",
-              "bathroom" = "First Bathroom Law",
-              "sports" = "First Sports Law")
+################ Relative size of agg. post ATT vs. avg. pre ATTs
 
-ggplot(data = df_coeffs,
-       aes(x = overall_att,
-           y = case_match(model,
-                      "trans_nsg" ~ "Trans Comparison Group,\nNo Small Groups",
-                      "trans_cov" ~ "Trans Comparison Group,\nWith Covariates",
-                      "trans" ~ "Trans Comparison Group",
-                      "queer_nsg" ~ "Queer Comparison Group,\nNo Small Groups",
-                      "queer_cov" ~ "Queer Comparison Group,\nWith Covariates",
-                      "queer" ~ "Queer Comparison Group"))) +
-  geom_point() +
-  geom_segment(aes(x = overall_att + 1.96*overall_se,
-                   xend = overall_att - 1.96*overall_se)) +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  labs(x = "Overall ATT", y = "Model Specification",
-       title = "Overall ATTs and 95% CIs for Six Model Specifications of the Effect of \nPassage of a State's First Anti-Trans Law (Overall and by Type)\non the PHQ-4 Score of Trans/GNC Residents of that State") +
-  theme_minimal() +
-  theme(panel.background = element_rect(color = "black")) +
-  facet_wrap(law_type ~ ., labeller = as_labeller(lawnames), nrow = 1)
+coeffs$sports |> 
+  filter(t < -1) |> 
+  group_by(modelspec) |> 
+  summarize(overall_att = mean(overall_att),
+            avg_pre = mean(abs(att))) |> 
+  mutate(ratio = overall_att / avg_pre)
+
+coeffs$first |> 
+  filter(t < -1) |> 
+  group_by(modelspec) |> 
+  summarize(overall_att = mean(overall_att),
+            avg_pre = mean(abs(att))) |> 
+  mutate(ratio = overall_att / avg_pre)
+
+
+save(coeffs, file = here("did_results", "coeffs.Rdata"))
